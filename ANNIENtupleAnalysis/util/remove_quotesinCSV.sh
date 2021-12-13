b4185a7a07491f418f14a966771316a49cda19b6
edrakopo <edrakopo@staffmail.ed.ac.uk>
2021-12-13 11:16:30 +0200

    ANNIE data cleaning-keras classification

--- ANNIENtupleAnalysis/util/remove_quotesinCSV.sh
+++ ANNIENtupleAnalysis/util/remove_quotesinCSV.sh
@@ -1,3 +1,4 @@
  #sed -e 's/"//g' vars_DNN_Signal_Bkgd.csv > vars_DNN_Signal_Bkgd2.csv
-sed -e 's/"//g' vars_DNN_Signal_BkgdNEW.csv > vars_DNN_Signal_BkgdNEW2.csv
+#sed -e 's/"//g' vars_DNN_Signal_BkgdNEW.csv > vars_DNN_Signal_BkgdNEW2.csv
 #sed -e 's/"//g' vars_DNN_Signal_Bkgd_promptNEW2.csv > vars_DNN_Signal_Bkgd_promptNEW2.csv
+sed -e 's/"//g' vars_DNN_Signal_Bkgd_delNEW.csv > vars_DNN_Signal_Bkgd_delNEW2.csv  
